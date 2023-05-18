46bc5f2a49f885bc4f626564ce64279d6bada151
Andie Creel <andie.creel@gmail.com>
2023-05-18 14:59:47 -0400

    getting started

---
+++ 0.data_wrangle.R
@@ -0,0 +1,31 @@
+# Goal: Wrangle all the data
+# Andie Creel / Started May 18th
+
+library(ipumsr)
+library(dplyr)
+library(stringr)
+
+# -----------------------------------------------------------------------------
+# Load data downloaded from IPUMS (code provided by IPUMS)
+# atus_00002 only has 2017 - 2022
+# -----------------------------------------------------------------------------
+ddi <- read_ipums_ddi("raw_data/atus_00002.xml") #reads in code book
+myData <- read_ipums_micro(ddi) # reads in data 
+
+# -----------------------------------------------------------------------------
+# Initial cleaning: drop replicate 
+# -----------------------------------------------------------------------------
+
+# Codes I am using 
+
+# 1813.. : Travel Related to Sports, Exercise, and Recreation
+# 130000 : Sports, Exercise, and Recreation
+
+myWorking <- myData %>%
+  select(-starts_with("RWT")) %>% # dropping replicate weights
+  filter(YEAR == 2021) %>% # starting only with 
+  mutate(ACTIVITY = str_pad(ACTIVITY, width = 6, pad = "0", side = "left")) %>% #getting activity code back to 6 digits 
+  filter(str_starts(ACTIVITY, "1813") | str_starts(ACTIVITY, "130")) 
+  
+
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # atus_travel_cost
 This is for my first (and maybe multiple) dissertation chapter. Of interest: substitution between indoor and outdoor rec under different climate scenarios 
+
+
+## Data Wrangle 
+
+Codes that I need: 
+
+  - 1813.. : Travel Related to Sports, Exercise, and Recreation
+    - currently just taking all of travel time... need to think about. 
+  - 130000 : Sports, Exercise, and Recreation
+    - WILL need to go through this and get more specific (some aren't outdoors)
+
+NEXT STEP: 
+
+  - Get total amount of time traveling to recreate and recreation time. 
+  - solution to travel time: drop people on days who haven't done any outdoor rec activity (and then that non-correct rec travel time gets dropped too.)
