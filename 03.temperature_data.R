2a19ca3550a56dd13c1c76d1cf703179fee22556
Andie Creel <andie.creel@gmail.com>
2023-06-12 12:09:57 -0600

    started to get temperature to work

--- 03.temperature_data.R
+++ 03.temperature_data.R
@@ -4,16 +4,53 @@
 library(dplyr)
 library(vroom)
 library(countyweather)
+library(parallel)
+library(purrr)
+
+options(scipen = 999)
+options("noaakey" = Sys.getenv("noaakey"))
+
+# -----------------------------------------------------------------------------
+# get list of cbgs 
+# -----------------------------------------------------------------------------
+myTC <- vroom("clean_data/my_activity_travel_long.csv") 
+myDems <- vroom("clean_data/my_demographics.csv")
+
+myWeather <- left_join(myTC, myDems, by = "caseid") %>%
+  select(county, date) %>%
+  distinct() 
+
+myWeather$date <- as.Date(as.character(myWeather$date), format = "%Y%m%d")
 
 # -----------------------------------------------------------------------------
 # Get temperature data
 # -----------------------------------------------------------------------------
-andrew_precip <- daily_fips(fips = c("12086", "02001"), date_min = "1992-08-01", 
-                            date_max = "1992-08-31", var = "prcp")
-
-andrew_precip$daily_data$result
 
 #NEXT STEPS: run a loop through all firms and days i need
 
+result_list <- mclapply(1:5, function(i) {
+  tryCatch(
+    daily_fips(fips = myWeather$county[i],
+               date_min = myWeather$date[i], 
+               date_max = myWeather$date[i], 
+               var = "all"),
+    error = function(e) {
+      NA  # Return NA if an error occurs
+    }
+  )
+}, mc.cores = 4)
 
 
+
+# 
+# filtered_list <- discard(result_list, ~ na.omit(.x))
+# 
+# 
+# test<-result_list[!sapply(result_list, is.na)]
+# 
+# combined_df <- bind_rows(result_list[!sapply(result_list$daily_data$result, is.na)]$daily_data$result)
+# 
+# 
+# combined_df <- result_list %>%
+#   map_df(~ if (is.data.frame(.x)) .x else NA) %>%
+#   filter(!is.na(.[[1]]))
